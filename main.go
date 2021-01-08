b342688d4400f6a1812810147df838154614f373
Joshua Okobia <joshuaokobia@Joshuas-MacBook-Pro.local>
2021-01-08 22:46:24 +0100

    Updated application port

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -50,12 +51,11 @@ func main() {
 		fs.ServeHTTP(w, r)
 	})).Methods("GET")
 
-	// port := os.Getenv("PORT")
-	// addr := fmt.Sprintf("0.0.0.0:%s", port)
-	addr2 := "0.0.0.0:8000"
+	port := os.Getenv("PORT")
+	addr := fmt.Sprintf("0.0.0.0:%s", port)
 
 	srv := &http.Server{
-		Addr: addr2,
+		Addr: addr,
 		// Good practice to set timeouts to avoid Slowloris attacks.
 		WriteTimeout: time.Second * 30,
 		ReadTimeout:  time.Second * 30,
